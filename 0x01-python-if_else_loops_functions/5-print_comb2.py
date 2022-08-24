38c3a3d29f7f6fef7e176eaa9dd04fc2d3065a0b
ayoolaadewale <ayooladewale@yahoo.com>
2022-08-24 01:04:15 +0000

    change to 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,7 @@ for j in range(0, 100):
     if j < 10:
         print("0{}".format(j), end=', ')
     else:
-        print("{}".format(j), end=', ')
+        if j == 99:
+            print("{}".format(j))
+        else:
+            print("{}".format(j), end=', ')
