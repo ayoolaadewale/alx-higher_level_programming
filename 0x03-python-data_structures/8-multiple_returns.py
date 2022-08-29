6d4f3cea826d716d3cf241ebc06344e152b2854e
ayoolaadewale <ayooladewale@yahoo.com>
2022-08-29 22:14:17 +0000

    script 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence = '':
-        sentence[0] = None
-    return_value = (len(sentence), sentence[0])
-    return return_value
+    length = len(sentence)
+    value = sentence[0] if length > 0 else None
+    return (length, value)
