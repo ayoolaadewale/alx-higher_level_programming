8aa2cf027efb9655d2a1ed29aea26eb3695d8ccc
ayoolaadewale <ayooladewale@yahoo.com>
2022-08-30 17:15:58 +0000

    script 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+length = len(sys.argv)-1
+if length == 0:
+    print("0 arguments")
+else:
+    if length == 1:
+        print("{:d} argument:".format(length))
+        print("{:d}: {}".format(length, sys.argv[length]))
+    else:
+        print("{:d} arguments:".format(length))
+        for i in range(length):
+            print("{:d}: {}".format(i+1, sys.argv[i+1]))
