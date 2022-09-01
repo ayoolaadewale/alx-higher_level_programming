9e02ac4dbf92464ebdafacba0de225948ae2e30b
ayoolaadewale <ayooladewale@yahoo.com>
2022-09-01 04:14:13 +0000

    script 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-if __name == "__main__":
-    import sys
-    length = len(sys.argv)-1
-    if length == 0:
-        print("0 arguments.")
+def print_argv(argv):
+    n = len(argv)-1
+    if n == 0:
+        print("{:d}argument.".format(n))
+        return
     else:
-        if length == 1:
-            print("{:d} argument:".format(length))
-            print("{:d}: {}".format(length, sys.argv[length]))
+        if n == 1:
+            print("{:d} argument:".format(n))
         else:
-            print("{:d} arguments:".format(length))
-            for i in range(length):
-                print("{:d}: {}".format(i+1, sys.argv[i+1]))
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
+if __name__ == "__main__":
+    import sys
+    print_argv(sys.argv)
