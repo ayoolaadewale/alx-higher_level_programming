0cfdb2017a500b35feb653c099cd1cd26b59838c
ayoolaadewale <ayooladewale@yahoo.com>
2022-09-01 04:18:51 +0000

    script 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 def print_argv(argv):
     n = len(argv)-1
     if n == 0:
-        print("{:d}argument.".format(n))
+        print("{:d} argument.".format(n))
         return
     else:
         if n == 1:
@@ -13,6 +13,7 @@ def print_argv(argv):
         while i <= n:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
+
 
 if __name__ == "__main__":
     import sys
